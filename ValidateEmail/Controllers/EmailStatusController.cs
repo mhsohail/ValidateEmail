25b0dec2c10c627f4673094ebb62e4b81d945492
mhsohail <sohailx2x@yahoo.com>
2015-07-07 17:26:06 +0500

    CORS enabled

--- ValidateEmail/Controllers/EmailStatusController.cs
+++ ValidateEmail/Controllers/EmailStatusController.cs
@@ -5,35 +5,37 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Script.Serialization;
 using System.Xml;
 using ValidateEmail.Models;
 
 namespace ValidateEmail.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class EmailStatusController : ApiController
     {
         // GET: api/EmailStatus
-        public HttpResponseMessage Get(Account Account)
+        public HttpResponseMessage Get(string Email)
         {
             JavaScriptSerializer serializer = new JavaScriptSerializer();
-            var email = Account.FirstName + "." + Account.LastName + "@" + Account.DomainName;
-
-            string serviceUrl = "https://bpi.briteverify.com/emails.json?address=" + email + "&apikey=2194f75a-34ac-49d5-97c8-e321e508e58f";
+            
+            string serviceUrl = "https://bpi.briteverify.com/emails.json?address=" + Email + "&apikey=2194f75a-34ac-49d5-97c8-e321e508e58f";
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serviceUrl);
             request.Method = "GET";
-            request.ContentType = "application/json; charset=UTF-8";
             request.Accept = "application/json; charset=UTF-8";
 
             var httpResponse = (HttpWebResponse)request.GetResponse();
             EmailStatus EmailStatus = null;
+            string rt = string.Empty;
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
                 var responstText = streamReader.ReadToEnd();
+                rt = responstText;
                 EmailStatus = serializer.Deserialize<EmailStatus>(responstText);
             }
 
-            var HttpResponseMsg = Request.CreateResponse<EmailStatus>(HttpStatusCode.OK, EmailStatus);
+            HttpResponseMessage HttpResponseMsg = Request.CreateResponse<EmailStatus>(HttpStatusCode.OK, EmailStatus);
 
             return HttpResponseMsg;
         }
